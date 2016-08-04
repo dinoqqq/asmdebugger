1d8c993f3d25a5786d0e2851053e30e0e88032ce
Daan Kuijsten <daan@kuijstenwebcompany.nl>
2016-08-05 01:26:53 +0200

    added tests for mov, movsx, movzx

---
+++ test/spec/instructions/MovSpec.js
@@ -0,0 +1,107 @@
+describe("instruction mov:", function() {
+    beforeEach(function() {
+        Test.clear();
+    });
+
+    describe("basic", function() {
+        it("should mov reg32 to reg32", function() {
+            Test.code('mov eax, 1');
+            Test.code('mov ebx, eax');
+            Test.next(2);
+
+            expect($('.ebx.dec').text()).toEqual('1');
+        });
+
+        it("should mov reg16 to reg16", function() {
+            Test.code('mov ax, 2');
+            Test.code('mov bx, ax');
+            Test.next(2);
+
+            expect($('.bx.dec').text()).toEqual('2');
+        });
+
+        it("should mov reg8h to reg8h", function() {
+            Test.code('mov ah, 3');
+            Test.code('mov bh, ah');
+            Test.next(2);
+
+            expect($('.bh.dec').text()).toEqual('3');
+        });
+
+        it("should mov reg8l to reg8l", function() {
+            Test.code('mov al, 4');
+            Test.code('mov bl, al');
+            Test.next(2);
+
+            expect($('.bl.dec').text()).toEqual('4');
+        });
+
+        it("should mov reg8h to reg8l", function() {
+            Test.code('mov ah, 5');
+            Test.code('mov bl, ah');
+            Test.next(2);
+
+            expect($('.bl.dec').text()).toEqual('5');
+        });
+
+        it("should mov reg8l to reg8h", function() {
+            Test.code('mov al, 6');
+            Test.code('mov bh, al');
+            Test.next(2);
+
+            expect($('.bh.dec').text()).toEqual('6');
+        });
+
+        it("should mov value to reg32", function() {
+            Test.code('mov edx, 7');
+            Test.next();
+
+            expect($('.edx.dec').text()).toEqual('7');
+        });
+
+        it("should mov value to reg16", function() {
+            Test.code('mov dx, 8');
+            Test.next();
+
+            expect($('.dx.dec').text()).toEqual('8');
+        });
+
+        it("should mov value to reg8h", function() {
+            Test.code('mov dh, 9');
+            Test.next();
+
+            expect($('.dh.dec').text()).toEqual('9');
+        });
+
+        it("should mov value to reg8l", function() {
+            Test.code('mov dl, 10');
+            Test.next();
+
+            expect($('.dl.dec').text()).toEqual('10');
+        });
+
+        it("should not mov reg16 to reg32", function() {
+            Test.code('mov cx, 7');
+            Test.code('mov edx, cx');
+            Test.next(2);
+
+            expect(Test.getError()).toEqual('No instruction found for: mov edx cx');
+        });
+
+        it("should not mov reg8h to reg32", function() {
+            Test.code('mov ch, 7');
+            Test.code('mov edx, ch');
+            Test.next(2);
+
+            expect(Test.getError()).toEqual('No instruction found for: mov edx ch');
+        });
+
+        it("should not mov val to val", function() {
+            Test.code('mov 7, 8');
+            Test.next();
+
+            expect(Test.getError()).toEqual('No instruction found for: mov 7 8');
+        });
+    });
+});
+
---
+++ test/spec/instructions/MovsxSpec.js
@@ -0,0 +1,63 @@
+describe("instruction movzx:", function() {
+    beforeEach(function() {
+        Test.clear();
+    });
+
+    describe("basic", function() {
+        it("should movzx reg16 to reg32", function() {
+            Test.code('mov dx, 1111111100000000b');
+            Test.code('movzx eax, dx');
+            Test.next(2);
+
+            expect($('.eax.bin').text()).toEqual('00000000 00000000 11111111 00000000');
+        });
+
+        it("should movzx reg8h to reg32", function() {
+            Test.code('mov dh, 11111111b');
+            Test.code('movzx eax, dh');
+            Test.next(2);
+
+            expect($('.eax.bin').text()).toEqual('00000000 00000000 00000000 11111111');
+        });
+
+        it("should movzx reg8l to reg32", function() {
+            Test.code('mov dl, 11111111b');
+            Test.code('movzx eax, dl');
+            Test.next(2);
+
+            expect($('.eax.bin').text()).toEqual('00000000 00000000 00000000 11111111');
+        });
+
+        it("should movzx reg8h to reg16", function() {
+            Test.code('mov dh, 11111111b');
+            Test.code('movzx ax, dh');
+            Test.next(2);
+
+            expect($('.ax.bin').text()).toEqual('00000000 11111111');
+        });
+
+        it("should movzx reg8l to reg16", function() {
+            Test.code('mov dl, 11111111b');
+            Test.code('movzx ax, dl');
+            Test.next(2);
+
+            expect($('.ax.bin').text()).toEqual('00000000 11111111');
+        });
+
+        it("should not movzx reg16 to reg16", function() {
+            Test.code('mov dx, 11111111b');
+            Test.code('movzx ax, dx');
+            Test.next(2);
+
+            expect(Test.getError()).toEqual('No instruction found for: movzx ax dx');
+        });
+
+        it("should not movzx reg32 to reg8l", function() {
+            Test.code('mov eax, 11111111b');
+            Test.code('movzx dl, eax');
+            Test.next(2);
+
+            expect(Test.getError()).toEqual('No instruction found for: movzx dl eax');
+        });
+    });
+});
